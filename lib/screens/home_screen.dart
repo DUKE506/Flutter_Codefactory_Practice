2c05abbfc19e93462f1cfae2b79e75cb9ed30bbd
DUKE506 <dukebs1128@gmail.com>
2025-01-09 23:41:13 +0900

    feat : 랜덤숫자 생성 파트중

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:flutter_codefactory_practice/constant/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,15 +12,16 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[50],
+      backgroundColor: primaryColor,
       body: SafeArea(
-        top: true,
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              _Top(),
-              _Bottom(),
+              _Header(),
+              _Body(),
+              _Footer(),
             ],
           ),
         ),
@@ -30,82 +30,65 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class _Top extends StatefulWidget {
-  const _Top({super.key});
+class _Header extends StatelessWidget {
+  const _Header({super.key});
 
-  @override
-  State<_Top> createState() => _TopState();
-}
-
-class _TopState extends State<_Top> {
-  DateTime selectedDate = DateTime.now();
   @override
   Widget build(BuildContext context) {
-    final now = DateTime.now();
-
-    final textTheme = Theme.of(context).textTheme;
-    return Expanded(
-      child: Container(
-        child: Column(
-          children: [
-            Text(
-              'U&I',
-              style: textTheme.displayLarge,
-            ),
-            Text(
-              '우리 처음 만난 날',
-              style: textTheme.bodyLarge,
-            ),
-            Text(
-              '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
-              style: textTheme.bodyMedium,
-            ),
-            IconButton(
-              onPressed: () => {
-                showCupertinoDialog(
-                  context: context,
-                  barrierDismissible: true,
-                  builder: (constext) {
-                    return Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        color: Colors.white,
-                        height: 300,
-                        child: CupertinoDatePicker(
-                            mode: CupertinoDatePickerMode.date,
-                            dateOrder: DatePickerDateOrder.ymd,
-                            onDateTimeChanged: (DateTime date) => {}),
-                      ),
-                    );
-                  },
-                )
-              },
-              icon: const Icon(
-                Icons.favorite,
-                color: Colors.red,
-                size: 35,
-              ),
-            ),
-            Text(
-              'D+${selectedDate.difference(now).inDays + 1}',
-              style: textTheme.displayMedium,
-            )
-          ],
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤숫자 생성기',
+          style: TextStyle(color: Colors.white, fontSize: 30),
         ),
-      ),
+        IconButton(
+            onPressed: () => {},
+            icon: Icon(
+              Icons.settings,
+              color: redColor,
+            ))
+      ],
     );
   }
 }
 
-class _Bottom extends StatelessWidget {
-  const _Bottom({super.key});
+class _Body extends StatelessWidget {
+  const _Body({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        child: Image.asset('asset/img/middle_image.png'),
-      ),
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ]
+          .map((e) => Row(
+                children: e
+                    .map((number) => Text(
+                          number.toString(),
+                          style: TextStyle(color: Colors.white),
+                        ))
+                    .toList(),
+              ))
+          .toList(),
+    ));
+  }
+}
+
+class _Footer extends StatelessWidget {
+  const _Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => {},
+      style: ElevatedButton.styleFrom(
+          backgroundColor: redColor, foregroundColor: Colors.white),
+      child: Text('생성'),
     );
   }
 }
