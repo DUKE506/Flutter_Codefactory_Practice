8fd3cdd6150aba238d674ce96b4909d0f491bd87
duke <dukebs1128@gmail.com>
2025-01-16 16:32:39 +0900

    feat : 동영상플레이어 개발중

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_codefactory_practice/screens/home_screen.dart';
-import 'package:flutter_codefactory_practice/screens/route_one_screen.dart';
-import 'package:flutter_codefactory_practice/screens/route_three_screen.dart';
-import 'package:flutter_codefactory_practice/screens/route_two_screen.dart';
 
 void main() {
   // WidgetsFlutterBinding.ensureInitialized();
@@ -13,11 +10,6 @@ void main() {
         ///key 라우트 이름
         ///value - builder->이동하고싶은 라우트
         '/': (context) => HomeScreen(),
-        '/one': (context) => RouteOneScreen(
-              number: 1,
-            ),
-        '/two': (context) => RouteTwoScreen(),
-        '/three': (context) => RouteThreeScreen(),
       },
     ),
   );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_codefactory_practice/layout/default_layout.dart';
-import 'package:flutter_codefactory_practice/screens/route_one_screen.dart';
+import 'package:video_player/video_player.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,27 +9,102 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool showVideoPlayer = false;
+
   @override
   Widget build(BuildContext context) {
-    return DefaultLayout(
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: showVideoPlayer
+            ? _VideoPlayer()
+            : _VideoSelector(onLogoTap: onLogoTap));
+  }
+
+  onLogoTap() {}
+}
+
+class _VideoSelector extends StatelessWidget {
+  VoidCallback onLogoTap;
+  _VideoSelector({super.key, required this.onLogoTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xFF000118),
+        ],
+      )),
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(
+            onTap: onLogoTap,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          _Title(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  final VoidCallback onTap;
+  const _Logo({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.asset('asset/img/logo.png'),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  _Title({super.key});
+
+  final textStyle = const TextStyle(
+    color: Colors.white,
+    fontSize: 28,
+    fontWeight: FontWeight.w300,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        OutlinedButton(
-          onPressed: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return RouteOneScreen(
-                    number: 20,
-                  );
-                },
-              ),
-            );
-            print(result);
-          },
-          child: Text('Push!'),
+        Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
-      title: 'HomeScreen',
+    );
+  }
+}
+
+class _VideoPlayer extends StatelessWidget {
+  const _VideoPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('player'),
     );
   }
 }
